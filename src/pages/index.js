2c6df6aa53c0a69c66c1e137081d4d9fddc7098c
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-03-05 09:30:23 +0300

    feat: asdaf

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ const features = [
       <>
         Docusaurus was designed from the ground up to be easily installed and
         used to get your website up and running quickly.
+          asdadasdasasdadad
       </>
     ),
   },
