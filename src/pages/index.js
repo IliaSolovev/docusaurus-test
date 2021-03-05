3bfc5ad617407e2f1803a60fd40e3054ac9adf04
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-03-05 09:34:01 +0300

    feat: asdafsdfsdfsdf

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,6 @@ const features = [
       <>
         Docusaurus was designed from the ground up to be easily installed and
         used to get your website up and running quickly.
-          asdadasdasasdadad
       </>
     ),
   },
